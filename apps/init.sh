b2b15c3958d9883ded0629fb21c74d7dd7a0e99d
Marius Johnsen <marius.johnsen@outlook.com>
2025-03-02 21:09:42 +0100

    Update .shellsmith to the latest commit

--- apps/init.sh
+++ apps/init.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 apt_install() {
-  sudo apt update && sudo apt upgrade
-  sudo apt install curl git nodejs npm pipx wl-clipboard bat nemo nemo-fileroller \
-    dbus dbus-x11 xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gnome
+  sudo apt-get update
+  sudo apt-get full-upgrade -y
+
+  # Desktop utils
+  sudo apt-get install -y firefox-esr ranger
+  # System utils
+  sudo apt-get install -y network-manager
+  # CLI utils
+  sudo apt-get install -y  wget curl batcat git wl-clipboard
 }
+
+install_node_npm() {
+  echo "Installing Node.js LTS (22.x) and npm..."
+  curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+  sudo apt install -y nodejs
+} 
 
 git_config() {
   git config --global user.name "Marius Johnsen"
@@ -34,5 +46,6 @@ install_fonts() {
 }
 
 apt_install
+install_node_npm
 git_config
 install_fonts
