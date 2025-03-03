7846718da98160cb8b03488c0210aece101cd42c
Marius Johnsen <marius.johnsen@outlook.com>
2025-03-03 22:23:06 +0100

    remove common folder

--- common/dotfiles/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-local pyenv_prefix = vim.fn.system("pyenv prefix neovim"):gsub("\n", "")
-vim.g.python3_host_prog = pyenv_prefix .. "/bin/python"
-
-require("config.lazy")
-
-require("functions.create_ipynb")
