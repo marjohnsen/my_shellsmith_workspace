5c3e9efed4059f0ea1aa9dde44e337d67588efbb
Marius Johnsen <marius.johnsen@outlook.com>
2025-03-05 20:14:54 +0100

    add kitty install script

---
+++ apps/kitty.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+source "$SHELLSMITH_UTILS/safe_symlink.sh"
+
+install_kitty() {
+  sudo apt-get install kitty -y
+}
+
+configure_kitty() {
+  mkdir -p "$HOME/.config/kitty/"
+  safe_symlink "$SHELLSMITH_COMMON_DOTFILES/kitty" "$HOME/.config/kitty/kitty.conf"
+}
+
+configure_theme() {
+  mkdir -p "$HOME/.config/kitty"
+  curl https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/gruvbox_dark.conf >"$HOME/.config/kitty/theme.conf"
+}
+
+install_kitty
+configure_kitty
+configure_theme
