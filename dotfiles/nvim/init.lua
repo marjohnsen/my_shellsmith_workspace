4ea40acee228f07a8ef04939c996feda135925ad
Marius Johnsen <marius.johnsen@outlook.com>
2025-03-07 15:55:19 +0100

    fix neovim stuff

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,5 +1,5 @@
 local pyenv_prefix = vim.fn.system("pyenv prefix neovim"):gsub("\n", "")
-vim.g.python3_host_prog = pyenv_prefix .. "/bin/python"
+vim.g.python3_host_prog = pyenv_prefix .. "/bin/python3"
 
 require("config.lazy")
 
